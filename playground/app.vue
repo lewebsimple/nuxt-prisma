2fba7c68fb8c6fcd915eb13e608cd522d641e356
Pascal Martineau <pascal@lewebsimple.ca>
2024-03-21 18:55:59 -0400

    chore: Initial Nuxt module project

---
+++ playground/app.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    Nuxt Prisma playground!
+  </div>
+</template>
+
+<script setup>
+</script>
---
+++ src/module.ts
@@ -0,0 +1,15 @@
+import { defineNuxtModule, addPlugin, createResolver } from '@nuxt/kit'
+
+// Module options TypeScript interface definition
+export interface ModuleOptions {}
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: '@lewebsimple/nuxt-prisma',
+    configKey: 'prisma'
+  },
+  defaults: {},
+  setup (options, nuxt) {
+    const resolver = createResolver(import.meta.url)
+  }
+})
